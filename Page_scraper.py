b3c869f103bcfda70f68698eb5e420d9fec2beba
Ghulam Mustafa <sgocean25@gmail.com>
2021-02-27 08:05:23 +0500

    Define a function scrape-by-css

--- Page_scraper.py
+++ Page_scraper.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-import pandas as pd
+from selenium.common.exceptions import NoSuchElementException
 
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
@@ -11,12 +11,25 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 url = 'https://www.daraz.pk/smartphones/?spm=a2a0e.home.cate_1.1.6a274937HKedFN'
 driver.get(url)
 
-def scrape(css):
-    try:
-        elem = driver.find_element(By.CSS_SELECTOR, css).text
-        return elem
-    except Exception:
-        None
+def scrape_by_css(css,attr):
+    if attr == 's':
+        try:
+            em_txt = driver.find_element(By.CSS_SELECTOR, css).text
+        except:
+            try:
+                em_txt = driver.find_element(By.CSS_SELECTOR, css).get_attribute('innerHTML') 
+            except NoSuchElementException:
+                em_txt = None
+        return em_txt
+    else:
+        ems = driver.find_elements(By.CSS_SELECTOR, css)
+        for em in ems:
+            try:
+                em_txt = em.get_attribute('innerHTML')
+            except:
+                em_txt = em.text
+            return em_txt
+
 
 PD_links_list = []
 PD_links = driver.find_elements(By.CSS_SELECTOR,'.c2prKC .c16H9d>a')
@@ -25,23 +38,23 @@ for url in PD_links_list:
     driver.get(url)
     print('Product URL: ',url)
     PD_img = driver.find_element(By.CSS_SELECTOR,'.gallery-preview-panel__image').get_attribute('src')
-    print(PD_img)
-    PD_Title = scrape('.pdp-mod-product-badge-title') #driver.find_element(By.CSS_SELECTOR,'.pdp-mod-product-badge-title').text
+    print('Image URL',PD_img)
+    PD_Title = scrape_by_css('.pdp-mod-product-badge-title','s')
     print(PD_Title)
-    PD_Price = scrape('span.pdp-price_size_xl')# driver.find_element(By.CSS_SELECTOR,'span.pdp-price_size_xl').text
+    PD_Price = scrape_by_css('span.pdp-price_size_xl','s')
     print(PD_Price)
     # Ratings = driver.find_element(By.CSS_SELECTOR,'.pdp-stars_size_s+ .pdp-review-summary__link').text
-    Avg_Score = scrape('span.score-average')#driver.find_element(By.CSS_SELECTOR,'span.score-average').text
+    Avg_Score = scrape_by_css('span.score-average','s')
     print(Avg_Score)
-    Pv_Sel_Rat = scrape('.rating-positive')#driver.find_element(By.CSS_SELECTOR,'.rating-positive').text
+    Pv_Sel_Rat = scrape_by_css('.rating-positive','s')
     print(Pv_Sel_Rat)
-    Avg_Shiping = scrape('.info-content:nth-child(2) .seller-info-value')#driver.find_element(By.CSS_SELECTOR,'.info-content:nth-child(2) .seller-info-value').text
+    Avg_Shiping = scrape_by_css('.info-content:nth-child(2) .seller-info-value','s')
     print(Avg_Shiping)
-    Avg_Chat_res = scrape('.info-content:nth-child(3) .seller-info-value')#driver.find_element(By.CSS_SELECTOR,'.info-content:nth-child(3) .seller-info-value').text
+    Avg_Chat_res = scrape_by_css('.info-content:nth-child(3) .seller-info-value','s')
     print(Avg_Chat_res)
-    Delivery_Time = scrape('.delivery-option-item__time')#driver.find_element(By.CSS_SELECTOR,'.delivery-option-item__time').text
+    Delivery_Time = scrape_by_css('.delivery-option-item__time','s')
     print(Delivery_Time)
-    Sold_by = scrape('.seller-name__detail-name')#driver.find_element(By.CSS_SELECTOR,'.seller-name__detail-name').text
+    Sold_by = scrape_by_css('.seller-name__detail-name','s')
     print(Sold_by)
 
 
